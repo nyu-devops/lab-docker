d43a445d2848ff6a90d608c97dc8147e4384bb19
John Rofrano <johnnyroy@johnrofrano.com>
2022-04-13 14:55:18 -0400

    changed port to 8080

--- .devcontainer/scripts/setup.sh
+++ .devcontainer/scripts/setup.sh
@@ -2,5 +2,5 @@
 echo "Setting up Docker lab environment..."
 docker pull alpine
 docker pull python:3.9-slim
-docker run -d --name redis -p 6379:6379 -v redis:/data redis:6-alpine
+docker run --restart=always -d --name redis -p 6379:6379 -v redis:/data redis:6-alpine
 echo "Setup complete"
