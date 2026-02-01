428e59fc0b403a5dd09b1c94fb69444e6e6bf0b5
John Rofrano <johnnyroy@johnrofrano.com>
2026-02-01 14:40:50 -0500

    Updated lab tio Python 3.12

--- .devcontainer/scripts/setup-lab.sh
+++ .devcontainer/scripts/setup-lab.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "Setting up Docker lab environment..."
 docker pull redis:6-alpine
-docker pull python:3.11-slim
+docker pull python:3.12-slim
 docker run --restart=always -d --name redis -p 6379:6379 -v redis:/data redis:6-alpine
 echo "Setup complete"
